3fdca8ff6de5b5d9b459ac553b01841ff05c1af1
Shawn Levin <shawnlevin22@gmail.com>
2018-02-11 20:35:58 -0800

    blogDemo started

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import Axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?Key=shawn';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    };
+
+}
