359945ad2d2109bd55e1dceafa3fab4a06611d91
Shawn Levin <shawnlevin22@gmail.com>
2018-04-10 10:50:13 -0700

    added blog pages

--- src/components/postsIndex.js
+++ src/components/postsIndex.js
@@ -1,7 +1,9 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
-import _ from 'lodash';
+
 
 class PostsIndex extends Component {
 
@@ -10,20 +12,22 @@ class PostsIndex extends Component {
     }
 
     renderPosts() {
-        _.map(this.props.posts, post => {
+        return _.map(this.props.posts, post => {
             return (
                 <li className='list-group-item' key={post.id}>
                     {post.title}
-                    go
                 </li>
             );
         });
     }
 
     render() {
-        console.log(this.props.posts)
+        console.log("posts in render", this.props.posts)
         return (
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">Add A Post</Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
-
 import reducers from './reducers';
 import PostsIndex from './components/postsIndex';
 import PostsNew from './components/posts_new';
@@ -15,8 +14,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path='/' component={PostsIndex} />
-        <Route path='/posts/new' component={PostsNew} />
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
